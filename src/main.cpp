55a8f91b9985320f59334811bf039ec423ff0805
ohel <ohel@users.noreply.github.com>
2017-07-16 04:52:04 +0300

    Better version using a real web server lib.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "Arduino.h"
 #include "ESP8266WiFi.h"
+#include "ESP8266WebServer.h"
  
 #include "ssid.h"
 #include "password.h"
@@ -10,20 +11,13 @@ char* index_html = reinterpret_cast<char*>(&src_index_html[0]);
 #define LED_OFF HIGH
  
 int ledPin = 2; // GPIO2
-WiFiServer server(80);
+ESP8266WebServer server(80);
  
 void setup() {
     Serial.begin(115200);
-    delay(10);
- 
     pinMode(ledPin, OUTPUT);
-    digitalWrite(ledPin, LED_OFF);
-    digitalWrite(ledPin, LED_ON);
-    delay(1000);
-    digitalWrite(ledPin, LED_OFF);
-     
-    Serial.println();
-    Serial.print("Connecting to ");
+
+    Serial.print("Connecting to: ");
     Serial.println(ssid);
      
     WiFi.begin(ssid, password);
@@ -33,63 +27,33 @@ void setup() {
         Serial.print(".");
     }
     Serial.println("");
-    Serial.println("WiFi connected");
+    Serial.print("WiFi connected using IP address: ");
+    Serial.println(WiFi.localIP());
      
+    server.on("/", [](){
+        Serial.println("Request: /");
+        server.send(200, "text/html", index_html);
+    });
+    server.on("/favicon.ico", [](){
+        Serial.println("Request: /favicon.ico");
+        server.send(404);
+    });
+    server.on("/pulse", [](){
+        Serial.println("Request: /pulse");
+        server.send(200, "text/html", index_html);
+        digitalWrite(ledPin, LED_ON);
+        delay(250);
+        digitalWrite(ledPin, LED_OFF);
+    });
+
     server.begin();
-    Serial.println("Server started");
-    digitalWrite(ledPin, LED_ON);
-    delay(500);
+    Serial.println("Server is running.");
+    digitalWrite(ledPin, LED_ON); delay(250);
+    digitalWrite(ledPin, LED_OFF); delay(250);
+    digitalWrite(ledPin, LED_ON); delay(250);
     digitalWrite(ledPin, LED_OFF);
-    delay(500);
-    digitalWrite(ledPin, LED_ON);
-    delay(500);
-    digitalWrite(ledPin, LED_OFF);
- 
-    Serial.print("Use this URL to connect: ");
-    Serial.print("http://");
-    Serial.print(WiFi.localIP());
-    Serial.println("/");
-        
 }
  
 void loop() {
-    WiFiClient client = server.available();
-    if (!client) {
-        return;
-    }
-     
-    while(!client.available()){
-        delay(1);
-    }
-     
-    String request = client.readStringUntil('\r');
-    Serial.println(request);
-    client.flush();
-     
-    if (request.indexOf("/favicon.ico") != -1) {
-        client.println("HTTP/1.1 204 No Content");
-        return;
-    }
-
-    int value = LED_OFF;
-    if (request.indexOf("/led") != -1) {
-        value = LED_ON;
-    } 
-    digitalWrite(ledPin, value);
- 
-    client.println("HTTP/1.1 200 OK");
-    client.println("Content-Type: text/html");
-    client.println("");
-    client.println("<!DOCTYPE HTML>");
-    client.println("<html>");
-    client.print("Led pin is now: ");
-    if(value == LED_ON) {
-        client.print("On");    
-    } else {
-        client.print("Off");
-    }
-    client.println("<br><br>");
-    client.println("Click <a href=\"/led\">here</a> turn the LED on.<br>");
-    client.println("Click <a href=\"/\">here</a> turn the LED off.<br>");
-    client.println("</html>");
+    server.handleClient();
 }
